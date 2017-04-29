ec0f5bf3c6b9fecca9d06530ba927141edfda640
Jim Pepe <jim.pepe@verizon.net>
2017-04-29 10:28:05 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/style/Style.java
@@ -0,0 +1,23 @@
+package com.course.example.style;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Style extends Activity {
+    
+	private TextView colors;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        colors = (TextView)findViewById(R.id.colored);
+        
+        int color = getResources().getColor(R.color.orange);
+		colors.setTextColor(color);
+		colors.setText("Hello World");
+
+    } 
+}
